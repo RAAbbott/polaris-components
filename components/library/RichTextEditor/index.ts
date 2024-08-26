801e3a91408e2d225d51723759d98fc0fc750a8e
basil <basilv@gmail.com>
2024-08-26 15:51:26 +0800

    Removal of useEffect to load to Quill and various other updates for new RichTextEditor component

--- components/library/RichTextEditor/index.ts
+++ components/library/RichTextEditor/index.ts
@@ -2,5 +2,5 @@
 export { tabs } from "./tabs";
 export { Preview } from "./Preview/index";
 export const title = "Rich Text Editor";
-export const contributor = "Evan Forge"
-export const dependencies = ['react-quill','remix-utils/client-only'];
+export const contributor = "fabregas4"
+export const dependencies = ['react-quill'];
