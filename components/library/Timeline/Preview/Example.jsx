04e11723b045cf24d4f898e3e15e237a470adfcf
RAAbbott <a.abbott2596@gmail.com>
2024-09-24 23:35:14 -0700

    added mobile padding to Timeline

--- components/library/Timeline/Preview/Example.jsx
+++ components/library/Timeline/Preview/Example.jsx
@@ -1,8 +1,7 @@
-import { Page, Badge } from '@shopify/polaris';
-import Timeline from './Timeline'
+import { Page, Badge, Layout, Box, Card } from '@shopify/polaris';
+import Timeline from './Timeline';
 
 export function Example() {
-
   // Event timestamps must be in date order (ascending or descending)
   // tone is optional
   //  - critical and caution will use Alert indicator
@@ -26,7 +25,7 @@ export function Example() {
     },
     {
       tone: 'base',
-      icon: <img src="/timeline-icon_ricemill.png" width="16" height="16" />,
+      icon: <img src='/timeline-icon_ricemill.png' width='16' height='16' />,
       url: undefined,
       timelineEvent: (
         <>
@@ -58,27 +57,19 @@ export function Example() {
     {
       tone: 'base',
       url: undefined,
-      timelineEvent: (
-        <>
-          Customer logged in.
-        </>
-      ),
+      timelineEvent: <>Customer logged in.</>,
       timestamp: new Date('2024-09-11T09:44:00')
     },
     {
       tone: 'base',
       url: undefined,
-      timelineEvent: (
-        <>
-          Failed login attempt detected.
-        </>
-      ),
+      timelineEvent: <>Failed login attempt detected.</>,
       timestamp: new Date('2024-09-11T06:59:00')
     },
     {
       tone: 'base',
       url: undefined,
-      icon: <img src="/timeline-icon_loyalty.png" width="16" height="16" />,
+      icon: <img src='/timeline-icon_loyalty.png' width='16' height='16' />,
       timelineEvent: (
         <>
           Customer redeemed 50 reward points on an order <Badge>#1237</Badge> (LoyaltyPlus)
@@ -89,22 +80,16 @@ export function Example() {
     {
       tone: 'base',
       url: undefined,
-      icon: <img src="/timeline-icon_loyalty.png" width="16" height="16" />,
+      icon: <img src='/timeline-icon_loyalty.png' width='16' height='16' />,
       timelineEvent: (
-        <>
-          Customer earned 100 reward points for subscribing to your mailing list. (LoyaltyPlus)
-        </>
+        <>Customer earned 100 reward points for subscribing to your mailing list. (LoyaltyPlus)</>
       ),
       timestamp: new Date('2024-09-10T18:14:00')
     },
     {
       tone: 'caution',
       url: undefined,
-      timelineEvent: (
-        <>
-          Account flagged for unusual activity.
-        </>
-      ),
+      timelineEvent: <>Account flagged for unusual activity.</>,
       timestamp: new Date('2024-09-10T16:00:00')
     },
     {
@@ -140,90 +125,62 @@ export function Example() {
     {
       tone: 'base',
       url: 'https://example.com/points-earned',
-      timelineEvent: (
-        <>
-          Customer earned 200 reward points after purchase.
-        </>
-      ),
+      timelineEvent: <>Customer earned 200 reward points after purchase.</>,
       timestamp: new Date('2024-09-09T13:00:00')
     },
     {
       tone: 'base',
       url: undefined,
-      timelineEvent: (
-        <>
-          Customer earned 100 reward points.
-        </>
-      ),
+      timelineEvent: <>Customer earned 100 reward points.</>,
       timestamp: new Date('2024-09-09T11:30:00')
     },
     {
       tone: 'base',
       url: undefined,
-      timelineEvent: (
-        <>
-          Customer contacted support regarding an issue with order.
-        </>
-      ),
+      timelineEvent: <>Customer contacted support regarding an issue with order.</>,
       timestamp: new Date('2024-09-09T11:00:00')
     },
     {
       tone: 'critical',
       url: undefined,
-      icon: <img src="/timeline-icon_security.png" width="16" height="16" />,
-      timelineEvent: (
-        <>
-          Customer flagged for fraud. (Securité)
-        </>
-      ),
+      icon: <img src='/timeline-icon_security.png' width='16' height='16' />,
+      timelineEvent: <>Customer flagged for fraud. (Securité)</>,
       timestamp: new Date('2024-09-09T08:15:00')
     },
     {
       tone: 'base',
       url: undefined,
-      icon: <img src="/timeline-icon_ricemill.png" width="16" height="16" />,
-      timelineEvent: (
-        <>
-          Customer updated their shipping address. (Ricemill)
-        </>
-      ),
+      icon: <img src='/timeline-icon_ricemill.png' width='16' height='16' />,
+      timelineEvent: <>Customer updated their shipping address. (Ricemill)</>,
       timestamp: new Date('2024-09-08T14:09:00')
     },
     {
       tone: 'base',
       url: undefined,
-      timelineEvent: (
-        <>
-          Customer&apos;s email address was updated.
-        </>
-      ),
+      timelineEvent: <>Customer&apos;s email address was updated.</>,
       timestamp: new Date('2024-09-08T12:44:00')
     },
     {
       tone: 'base',
       url: 'https://example.com/account-updated',
-      timelineEvent: (
-        <>
-          Account details updated.
-        </>
-      ),
+      timelineEvent: <>Account details updated.</>,
       timestamp: new Date('2024-09-08T11:19:00')
     },
     {
       tone: 'base',
       url: undefined,
-      timelineEvent: (
-        <>
-          Customer subscribed to newsletter.
-        </>
-      ),
+      timelineEvent: <>Customer subscribed to newsletter.</>,
       timestamp: new Date('2024-09-07T09:29:00')
     }
-  ]
+  ];
 
   return (
     <Page narrowWidth>
-      <Timeline items={timelineItems} />
+      <Layout>
+        <Layout.Section>
+          <Timeline items={timelineItems} />
+        </Layout.Section>
+      </Layout>
     </Page>
   );
 }
--- components/library/Timeline/tabs.ts
+++ components/library/Timeline/tabs.ts
@@ -1,10 +1,9 @@
 import { Tab } from '@/types';
 
-const Example = `import { Page, Badge } from '@shopify/polaris';
-import Timeline from './Timeline'
+const Example = `import { Page, Badge, Layout, Box, Card } from '@shopify/polaris';
+import Timeline from './Timeline';
 
 export function Example() {
-
   // Event timestamps must be in date order (ascending or descending)
   // tone is optional
   //  - critical and caution will use Alert indicator
@@ -28,7 +27,7 @@ export function Example() {
     },
     {
       tone: 'base',
-      icon: <img src="/timeline-icon_ricemill.png" width="16" height="16" />,
+      icon: <img src='/timeline-icon_ricemill.png' width='16' height='16' />,
       url: undefined,
       timelineEvent: (
         <>
@@ -60,27 +59,19 @@ export function Example() {
     {
       tone: 'base',
       url: undefined,
-      timelineEvent: (
-        <>
-          Customer logged in.
-        </>
-      ),
+      timelineEvent: <>Customer logged in.</>,
       timestamp: new Date('2024-09-11T09:44:00')
     },
     {
       tone: 'base',
       url: undefined,
-      timelineEvent: (
-        <>
-          Failed login attempt detected.
-        </>
-      ),
+      timelineEvent: <>Failed login attempt detected.</>,
       timestamp: new Date('2024-09-11T06:59:00')
     },
     {
       tone: 'base',
       url: undefined,
-      icon: <img src="/timeline-icon_loyalty.png" width="16" height="16" />,
+      icon: <img src='/timeline-icon_loyalty.png' width='16' height='16' />,
       timelineEvent: (
         <>
           Customer redeemed 50 reward points on an order <Badge>#1237</Badge> (LoyaltyPlus)
@@ -91,22 +82,16 @@ export function Example() {
     {
       tone: 'base',
       url: undefined,
-      icon: <img src="/timeline-icon_loyalty.png" width="16" height="16" />,
+      icon: <img src='/timeline-icon_loyalty.png' width='16' height='16' />,
       timelineEvent: (
-        <>
-          Customer earned 100 reward points for subscribing to your mailing list. (LoyaltyPlus)
-        </>
+        <>Customer earned 100 reward points for subscribing to your mailing list. (LoyaltyPlus)</>
       ),
       timestamp: new Date('2024-09-10T18:14:00')
     },
     {
       tone: 'caution',
       url: undefined,
-      timelineEvent: (
-        <>
-          Account flagged for unusual activity.
-        </>
-      ),
+      timelineEvent: <>Account flagged for unusual activity.</>,
       timestamp: new Date('2024-09-10T16:00:00')
     },
     {
@@ -142,109 +127,85 @@ export function Example() {
     {
       tone: 'base',
       url: 'https://example.com/points-earned',
-      timelineEvent: (
-        <>
-          Customer earned 200 reward points after purchase.
-        </>
-      ),
+      timelineEvent: <>Customer earned 200 reward points after purchase.</>,
       timestamp: new Date('2024-09-09T13:00:00')
     },
     {
       tone: 'base',
       url: undefined,
-      timelineEvent: (
-        <>
-          Customer earned 100 reward points.
-        </>
-      ),
+      timelineEvent: <>Customer earned 100 reward points.</>,
       timestamp: new Date('2024-09-09T11:30:00')
     },
     {
       tone: 'base',
       url: undefined,
-      timelineEvent: (
-        <>
-          Customer contacted support regarding an issue with order.
-        </>
-      ),
+      timelineEvent: <>Customer contacted support regarding an issue with order.</>,
       timestamp: new Date('2024-09-09T11:00:00')
     },
     {
       tone: 'critical',
       url: undefined,
-      icon: <img src="/timeline-icon_security.png" width="16" height="16" />,
-      timelineEvent: (
-        <>
-          Customer flagged for fraud. (Securité)
-        </>
-      ),
+      icon: <img src='/timeline-icon_security.png' width='16' height='16' />,
+      timelineEvent: <>Customer flagged for fraud. (Securité)</>,
       timestamp: new Date('2024-09-09T08:15:00')
     },
     {
       tone: 'base',
       url: undefined,
-      icon: <img src="/timeline-icon_ricemill.png" width="16" height="16" />,
-      timelineEvent: (
-        <>
-          Customer updated their shipping address. (Ricemill)
-        </>
-      ),
+      icon: <img src='/timeline-icon_ricemill.png' width='16' height='16' />,
+      timelineEvent: <>Customer updated their shipping address. (Ricemill)</>,
       timestamp: new Date('2024-09-08T14:09:00')
     },
     {
       tone: 'base',
       url: undefined,
-      timelineEvent: (
-        <>
-          Customer&apos;s email address was updated.
-        </>
-      ),
+      timelineEvent: <>Customer&apos;s email address was updated.</>,
       timestamp: new Date('2024-09-08T12:44:00')
     },
     {
       tone: 'base',
       url: 'https://example.com/account-updated',
-      timelineEvent: (
-        <>
-          Account details updated.
-        </>
-      ),
+      timelineEvent: <>Account details updated.</>,
       timestamp: new Date('2024-09-08T11:19:00')
     },
     {
       tone: 'base',
       url: undefined,
-      timelineEvent: (
-        <>
-          Customer subscribed to newsletter.
-        </>
-      ),
+      timelineEvent: <>Customer subscribed to newsletter.</>,
       timestamp: new Date('2024-09-07T09:29:00')
     }
-  ]
+  ];
 
   return (
     <Page narrowWidth>
-      <Timeline items={timelineItems} />
+      <Layout>
+        <Layout.Section>
+          <Timeline items={timelineItems} />
+        </Layout.Section>
+      </Layout>
     </Page>
   );
 }
 `;
 
-const Timeline = `import { BlockStack, Box, Icon, InlineGrid, InlineStack, Link, Text } from "@shopify/polaris";
-import { Fragment } from "react"
-import { AlertCircleIcon, CheckCircleIcon, ChevronRightIcon, CircleChevronRightIcon } from "@shopify/polaris-icons";
+const Timeline = `import { BlockStack, Box, Icon, InlineGrid, InlineStack, Link, Text } from '@shopify/polaris';
+import { Fragment } from 'react';
+import {
+  AlertCircleIcon,
+  CheckCircleIcon,
+  ChevronRightIcon,
+  CircleChevronRightIcon
+} from '@shopify/polaris-icons';
 import styles from './timeline.module.css';
 
 export default function Timeline({ items }) {
-
   function getBulletIconFromTone(tone) {
     switch (tone) {
       case 'critical':
       case 'caution':
         return AlertCircleIcon;
       case 'success':
-        return CheckCircleIcon
+        return CheckCircleIcon;
       case 'base':
       case undefined:
         return null;
@@ -256,66 +217,77 @@ export default function Timeline({ items }) {
   let lastDate = null;
 
   return (
-    <Box>
-      <BlockStack gap="300">
-        {items?.length ? items.map((item, index) => {
-          const currentDate = item.timestamp.toLocaleDateString([], { year: 'numeric', month: 'long', day: 'numeric' });
-          const showDate = currentDate !== lastDate;
-          lastDate = currentDate;
-          const bulletIcon = getBulletIconFromTone(item.tone);
+    <Box paddingInline={{ xs: '300', md: '0' }}>
+      <BlockStack gap='300'>
+        {items?.length ? (
+          items.map((item, index) => {
+            const currentDate = item.timestamp.toLocaleDateString([], {
+              year: 'numeric',
+              month: 'long',
+              day: 'numeric'
+            });
+            const showDate = currentDate !== lastDate;
+            lastDate = currentDate;
+            const bulletIcon = getBulletIconFromTone(item.tone);
 
-          return (
-            <Fragment key={index}>
-              {showDate && (
-                <InlineGrid gap="200" columns="30px auto 90px">
-                  <div>
-                    &nbsp;
-                  </div>
-                  <BlockStack gap="0">
-                    <Box paddingBlockStart="600">
-                      <Text alignment="start" as="h2" variant="bodySm" tone="subdued" key={\`date-\${index}\`}>
-                        {currentDate}
-                      </Text>
-                    </Box>
-                  </BlockStack>
-                  <div>&nbsp;</div>
-                </InlineGrid>
+            return (
+              <Fragment key={index}>
+                {showDate && (
+                  <InlineGrid gap='200' columns='30px auto 90px'>
+                    <div>&nbsp;</div>
+                    <BlockStack gap='0'>
+                      <Box paddingBlockStart='600'>
+                        <Text alignment='start' as='h2' variant='bodySm' tone='subdued'>
+                          {currentDate}
+                        </Text>
+                      </Box>
+                    </BlockStack>
+                    <div>&nbsp;</div>
+                  </InlineGrid>
+                )}
 
-              )}
-
-              <InlineGrid gap="200" columns="30px auto 90px" alignItems="center">
-                <div className={styles['timeline-icon']}>
-                  {item.tone === 'base' || !bulletIcon ? (
-                    <div className={styles['timeline-icon-base']}>
-                      <div className={styles['timeline-icon-base-inner']} />
-                    </div>
-                  ) : (
-                    <span className={styles['timeline-icon-polaris-icon']}><Icon source={bulletIcon} tone={item.tone} /></span>
-                  )}
-                </div>
-                <Box className={styles['timeline-event-description']}>
-                  <InlineStack gap="200" wrap={false} blockAlign="center">
-                    {item.icon}
-                    {item.url ? (
-                      <Link url={item.url} monochrome removeUnderline>
-                        <InlineStack gap="0" wrap={false} blockAlign="start">
-                          <Box className={styles['timeline-event-link-main']}>{item.timelineEvent}</Box>
-                          <Icon source={ChevronRightIcon} />
-                        </InlineStack>
-                      </Link>
+                <InlineGrid gap='200' columns='30px auto 90px' alignItems='center'>
+                  <div className={styles['timeline-icon']}>
+                    {item.tone === 'base' || !bulletIcon ? (
+                      <div className={styles['timeline-icon-base']}>
+                        <div className={styles['timeline-icon-base-inner']} />
+                      </div>
                     ) : (
-                      <Box>{item.timelineEvent}</Box>
+                      <span className={styles['timeline-icon-polaris-icon']}>
+                        <Icon source={bulletIcon} tone={item.tone} />
+                      </span>
                     )}
-                  </InlineStack>
-                </Box>
-                <Text as="p" alignment="end" tone="subdued" variant="bodySm">
-                  {item.timestamp.toLocaleTimeString([], { hour: 'numeric', minute: '2-digit', hour12: true })}
-                </Text>
-              </InlineGrid>
-            </Fragment>
-          );
-        }) : (
-          <Text as="p">No timeline events available.</Text>
+                  </div>
+                  <Box className={styles['timeline-event-description']}>
+                    <InlineStack gap='200' wrap={false} blockAlign='center'>
+                      {item.icon}
+                      {item.url ? (
+                        <Link url={item.url} monochrome removeUnderline>
+                          <InlineStack gap='0' wrap={false} blockAlign='start'>
+                            <Box className={styles['timeline-event-link-main']}>
+                              {item.timelineEvent}
+                            </Box>
+                            <Icon source={ChevronRightIcon} />
+                          </InlineStack>
+                        </Link>
+                      ) : (
+                        <Box>{item.timelineEvent}</Box>
+                      )}
+                    </InlineStack>
+                  </Box>
+                  <Text as='p' alignment='end' tone='subdued' variant='bodySm'>
+                    {item.timestamp.toLocaleTimeString([], {
+                      hour: 'numeric',
+                      minute: '2-digit',
+                      hour12: true
+                    })}
+                  </Text>
+                </InlineGrid>
+              </Fragment>
+            );
+          })
+        ) : (
+          <Text as='p'>No timeline events available.</Text>
         )}
       </BlockStack>
     </Box>
@@ -379,9 +351,8 @@ a:hover .timeline-event-link-main {
 }
 `;
 
-
 export const tabs: Tab[] = [
   { title: 'Example Usage', content: Example },
   { title: 'Timeline.jsx', content: Timeline },
-  { title: 'timeline.module.css', content: CSSFile, lang: 'css' },
+  { title: 'timeline.module.css', content: CSSFile, lang: 'css' }
 ];
