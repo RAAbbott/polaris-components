e47648405ab4846d619502f070dd584f597da39d
umar azeem <125313441+Umar-azeem@users.noreply.github.com>
2025-02-21 19:11:25 +0500

    Update index.ts

--- components/library/StatBox/index.ts
+++ components/library/StatBox/index.ts
@@ -1,6 +1,6 @@
 // Index files for each component directory should export the tabs, Component, and title
 
-import { Platform } from '@/types';
+import { Platform } from '@/types'; 
 
 export { tabs } from './tabs';
 export { Preview } from './Preview';
